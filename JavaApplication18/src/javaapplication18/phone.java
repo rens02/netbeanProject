e034b22400c80721c62cab70ad7c39ff060b4c76
rens02 <rens02@student.gunadarma.ac.id>
2022-06-02 12:48:03 +0700

    bantuin felix blm kelar

---
+++ JavaApplication18/src/javaapplication18/phone.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication18;
+
+/**
+ *
+ * @author ASUS
+ */
+class phone {
+    private String cord, type,address,brand ;
+    
+    public phone(String cord, String type,String address,String brand){
+        super();
+        this.cord = cord;
+        this.type = type;
+        this.address = address;
+        this.brand = brand;
+    }
+}
